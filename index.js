c55d00144bea85e07551e85c1f671ec6dd972b93
Azumi Callanan <azucm@hotmail.com>
2017-12-05 17:09:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph');
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob');
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift();
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop();
+  return kittens;
+}
+
+function appendKitten(name){
+  [name,...kittens];
+  return kittens;
+}
+
+function prependKitten(name){
+  [name,...kittens];
+  return kittens;
+}
+
+function removeLastKitten(name){
+  [name,...kittens];
+  return kittens;
+  
+function removeFirstKitten(name){
+  [name,...kittens];
+  return kittens;
